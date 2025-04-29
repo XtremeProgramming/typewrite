cd6e9981c061b31d5446cfb5fde3dbd59bdc42c8
Gustavo Monteiro <gustavosmonteiro@gmail.com>
2025-04-29 14:20:30 +0100

    chore(FE): add react-router v7 and base structure (#5)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { StrictMode } from "react";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
