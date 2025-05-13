1d08030480e850bb3ab8a344797d9c09bfca486d
Gustavo Monteiro <gustavosmonteiro@gmail.com>
2025-05-13 22:37:03 +0100

    feat(FE): add authentication forms with react-hook-form

--- frontend/src/components/RecoverPassword.tsx
+++ frontend/src/components/RecoverPassword.tsx
@@ -1,3 +1,63 @@
+import { Label } from "@radix-ui/react-label";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+
 export default function RecoverPassword() {
-  return <h1>Recover Password</h1>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className={"flex flex-col gap-6"}>
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-2xl">Recover password</CardTitle>
+              <CardDescription>
+                Enter your email to recover your password
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                      {...register("email")}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <Button type="submit" className="w-full">
+                      Recover password
+                    </Button>
+                  </div>
+                </div>
+                <div className="mt-4 text-center text-sm">
+                  Or you can{" "}
+                  <a href="/signin" className="underline underline-offset-4">
+                    go back to Login
+                  </a>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
