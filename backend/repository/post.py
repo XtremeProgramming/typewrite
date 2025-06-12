f27de7306398582f437e4b8156767db0f3907aed
kateoslv <kattyolv@gmail.com>
2025-06-12 11:49:59 +0100

    feat(BE): update post implementation

--- backend/repository/post.py
+++ backend/repository/post.py
@@ -4,12 +4,13 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from auth.dependencies import get_current_user
+from core.constants import POST_NOT_FOUND
 from models.post import Post
-from schemas.post import PostCreate
+from schemas.post import PostUpdate
 
 
 def create_post(
-    db: Session, post: PostCreate, user_id: str = Depends(get_current_user)
+    db: Session, post: Post, user_id: str = Depends(get_current_user)
 ) -> Post:
     db_post = Post(title=post.title, content=post.content, author_id=user_id)
 
@@ -21,3 +22,20 @@ def create_post(
 
 def get_post_by_id(db: Session, post_id: UUID) -> Post:
     return db.query(Post).filter(Post.id == post_id).first()
+
+
+def update_post(db: Session, post_id: UUID, update_data: PostUpdate) -> Post:
+    post = db.query(Post).filter(Post.id == post_id).first()
+
+    if not post:
+        raise ValueError(POST_NOT_FOUND)
+
+    if update_data.title is not None:
+        post.title = update_data.title
+
+    if update_data.content is not None:
+        post.content = update_data.content
+
+    db.commit()
+    db.refresh(post)
+    return post
