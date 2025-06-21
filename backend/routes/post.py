1dcbcb2d622ac7aeda4641d3c92b8fc8e6ddb712
Kate <48857255+kateoslv@users.noreply.github.com>
2025-06-21 11:34:38 +0100

    feat(BE): update post implementation (#35)
    * feat(BE): update post implementation
    
    * refactor(BE): change blog post update method from PATCH to PUT
    
    ---------
    
    Co-authored-by: kateoslv <kattyolv@gmail.com>

--- backend/routes/post.py
+++ backend/routes/post.py
@@ -9,15 +9,15 @@ from auth.dependencies import get_current_user
 from connection import get_db
 from core.constants import GENERIC_ERROR, POST_NOT_FOUND
 from models.user import User
-from repository.post import create_post, get_post_by_id
-from schemas.post import PostCreate, PostResponse
+from repository.post import create_post, get_post_by_id, update_post
+from schemas.post import Post, PostResponse
 
 router = APIRouter()
 
 
 @router.post("/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
 async def create(
-    post: PostCreate,
+    post: Post,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
@@ -45,3 +45,30 @@ async def get_post(post_id: UUID, db: Session = Depends(get_db)):
         )
 
     return PostResponse.model_validate(post)
+
+
+@router.put("/posts/{post_id}", response_model=PostResponse)
+async def update(
+    post_id: UUID,
+    update_data: Post,
+    db: Session = Depends(get_db),
+    User=Depends(get_current_user),
+):
+    try:
+        post = update_post(db, post_id, update_data)
+
+        return PostResponse.model_validate(post)
+
+    except ValueError:
+        db.rollback()
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"id": POST_NOT_FOUND},
+        )
+
+    except SQLAlchemyError:
+        db.rollback()
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"id": GENERIC_ERROR},
+        )
